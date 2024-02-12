02bfac5253815a4fc88c8ea576d189c53f867091
Laura Harper <lauraharper95@gmail.com>
2024-02-12 16:38:13 +0000

    added broken shower and fix shower functions

--- src/main/java/LeisureCentre.java
+++ src/main/java/LeisureCentre.java
@@ -6,12 +6,20 @@ public class LeisureCentre extends Building {
 
     private String typeOfPool;
     private int numberOfShowers;
-    private ArrayList<Equipment>;
+    private ArrayList<equipment>;
 
     public LeisureCentre(int numberOfRooms, int yearOfConstruction, boolean doubleGlazed,String typeOfPool,int numberOfShowers) {
         super(numberOfRooms, yearOfConstruction, doubleGlazed);
         this.typeOfPool = typeOfPool;
         this.numberOfShowers = numberOfShowers;
+    }
+
+    public int brokenShower() {
+        return this.numberOfShowers -= 1;
+    }
+
+    public int fixShower() {
+        return this.numberOfShowers += 1;
     }
 
 }
