233a841ed5096108b68722af7ca2f06b95d4cf66
Laura Harper <lauraharper95@gmail.com>
2024-02-12 16:10:59 +0000

    finished Cottage and Cottage Test

--- src/main/java/Cottage.java
+++ src/main/java/Cottage.java
@@ -4,7 +4,7 @@ public class Cottage extends Building {
     private boolean listedStatus;
     private int numberOfLogsOnFire;
 
-    public Cottage(int numberOfRooms, int yearOfConstruction, boolean doubleGlazed) {
+    public Cottage(int numberOfRooms, int yearOfConstruction, boolean doubleGlazed, int sizeOfGarden, boolean listedStatus, int numberOfLogsOnFire) {
         super(numberOfRooms, yearOfConstruction, doubleGlazed);
         this.sizeOfGarden = sizeOfGarden;
         this.numberOfLogsOnFire = numberOfLogsOnFire;
@@ -14,6 +14,18 @@ public class Cottage extends Building {
         return this.sizeOfGarden * 2;
     }
 
-    
+    public int getSizeOfGarden() {
+        return this.sizeOfGarden;
+    }
+
+    public int getNumberOfLogsOnFire(){
+        return this.numberOfLogsOnFire;
+    }
+
+    public int burnLogsOnFire(int numberOfLogsOnFire) {
+        numberOfLogsOnFire = 0;
+        return this.numberOfLogsOnFire = numberOfLogsOnFire;
+    }
+
 
 }
--- src/test/java/CottageTest.java
+++ src/test/java/CottageTest.java
@@ -1,2 +1,30 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class CottageTest {
+
+    Cottage cottage;
+
+    @BeforeEach
+    public void setUp() {
+        cottage = new Cottage(4, 1880, false, 20, true, 5);
+    }
+
+    @Test
+    public void canIncreaseSizeOfGarden(){
+       assertThat(cottage.increaseSizeOfGarden()).isEqualTo(40);
+  }
+
+    @Test
+    public void canGetNumberOfLogsOnFire(){
+        assertThat(cottage.getNumberOfLogsOnFire()).isEqualTo(5);
+    }
+
+    @Test
+    public void hasBurntLogsOnFire() {
+        assertThat(cottage.burnLogsOnFire(cottage.getNumberOfLogsOnFire())).isEqualTo(0);
+    }
+
 }
