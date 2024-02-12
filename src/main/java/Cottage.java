10d8f2c13cfa4cbcfd232868a60fb6ac12fefa33
Laura Harper <lauraharper95@gmail.com>
2024-02-12 15:11:42 +0000

    added increase garden size function

--- src/main/java/Cottage.java
+++ src/main/java/Cottage.java
@@ -6,5 +6,14 @@ public class Cottage extends Building {
 
     public Cottage(int numberOfRooms, int yearOfConstruction, boolean doubleGlazed) {
         super(numberOfRooms, yearOfConstruction, doubleGlazed);
+        this.sizeOfGarden = sizeOfGarden;
+        this.numberOfLogsOnFire = numberOfLogsOnFire;
     }
+
+    public int increaseSizeOfGarden() {
+        return this.sizeOfGarden * 2;
+    }
+
+    
+
 }
