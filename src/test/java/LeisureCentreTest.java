5e779f1d65009e89f7ed473369e8c090650b537f
Laura Harper <lauraharper95@gmail.com>
2024-02-12 16:44:25 +0000

    added test for leisurecentre

--- src/test/java/LeisureCentreTest.java
+++ src/test/java/LeisureCentreTest.java
@@ -1,5 +1,21 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class LeisureCentreTest {
 
+    LeisureCentre leisureCentre;
+
+    @BeforeEach
+    public void setUp() {
+        leisureCentre = new LeisureCentre(15, 1995, true, "infinity", 30);
+    }
+
+    @Test
+    public void showBrokenShower(){
+        assertThat(leisureCentre.numberOfShowers).isEqualTo(29);
+    }
 
 
 }
