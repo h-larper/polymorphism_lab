65390cfd644d3467fe9c549db1addfbc353771ba
Laura Harper <lauraharper95@gmail.com>
2024-02-13 14:06:34 +0000

    added method overload to Motorhome class

--- src/main/java/MotorHome.java
+++ src/main/java/MotorHome.java
@@ -1,2 +1,24 @@
 public class MotorHome {
+
+    private String name;
+    private int yearOfManufacture;
+    private String licensePlate;
+
+    public MotorHome(String name, int yearOfManufacture, String licensePlate) {
+        this.name = name;
+        this.yearOfManufacture = yearOfManufacture;
+        this.licensePlate = licensePlate;
+    }
+
+    public int calculateDevaluation(int current, int original) {
+        return original - current;
+    }
+
+    public double calculateDevaluation(double current, double original) {
+        return original - current;
+    }
+
+
+
+
 }
