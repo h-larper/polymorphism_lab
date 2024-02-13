4ff71db52ce724ccae18c3af9f2408e0ae4e8e8c
Laura Harper <lauraharper95@gmail.com>
2024-02-13 14:12:55 +0000

    added MotorhomeTest Test for canDevaluate

--- src/test/java/MotorHomeTest.java
+++ src/test/java/MotorHomeTest.java
@@ -1,2 +1,20 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class MotorHomeTest {
+
+    MotorHome motorhome;
+
+    @BeforeEach
+    public void setUp() {
+        motorhome = new MotorHome("Happy Camper", 1993, "GR8 FUN");
+    }
+
+    @Test
+    public void canCalculateDevaluation() {
+        assertThat(motorhome.calculateDevaluation(1500, 2000)).isEqualTo(500);
+    }
+
 }
